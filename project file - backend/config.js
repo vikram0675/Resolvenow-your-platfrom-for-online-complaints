b776f45f0f37dcf88d1260f4d5da7461b53e75b5
Ajith Pusuluri <228x1a0583@khitguntur.ac.in>
2025-06-28 18:45:54 +0530

    Delete project file - backend directory

--- project file - backend/config.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb+srv://Mohan:er2af5Q1UBCewcmZ@cluster0.di6bzxd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(()=>{
-   console.log("connected to mongoDB Atlas")
-})
